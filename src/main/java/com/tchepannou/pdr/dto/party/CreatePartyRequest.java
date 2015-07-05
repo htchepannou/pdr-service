a4ef6f074be90f9304a43d5c602cb40596fdc3cf
htchepannou <htchepannou@expedia.com>
2015-07-05 12:23:24 -0400

    code improvement

--- src/main/java/com/tchepannou/pdr/dto/party/CreatePartyRequest.java
+++ src/main/java/com/tchepannou/pdr/dto/party/CreatePartyRequest.java
@@ -1,10 +1,12 @@
 package com.tchepannou.pdr.dto.party;
 
+import com.google.common.base.Joiner;
+import com.google.common.base.Strings;
 import org.hibernate.validator.constraints.NotBlank;
 
 public class CreatePartyRequest {
     //-- Attributes
-    @NotBlank
+    @NotBlank(message = "kind")
     private String kind;
 
     private String name;
@@ -20,7 +22,14 @@ public class CreatePartyRequest {
         this.kind = kind;
     }
 
+    @NotBlank(message = "name")
     public String getName() {
+        if (Strings.isNullOrEmpty(name)){
+            name = Joiner
+                    .on(' ')
+                    .skipNulls()
+                    .join(firstName, lastName);
+        }
         return name;
     }
 
